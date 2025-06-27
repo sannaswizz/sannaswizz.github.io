e234803779ab37b8b03b4d860b75e404a7bd5701
sannaswizz <sannaswizz@gmail.com>
2025-06-27 23:45:06 +0300

    v1.0.6

---
+++ assets/20250618_en_harvesting-strawberries-l3yoAQdq.js
@@ -0,0 +1,29 @@
+import{j as e}from"./index-FOeX3u15.js";const s={title:"Magical Summer Moments",image:"../images/2025-06-27 23.17.57.jpg",tags:["life","writer"],previewText:"A warm day in the garden full of wild strawberries – it was exhausting, sweet, and unforgettable."};function r(a){const t={h1:"h1",p:"p",...a.components};return e.jsxs(e.Fragment,{children:[e.jsx(t.h1,{children:"Magical Summer Moments"}),`
+`,e.jsx("br",{}),`
+`,e.jsx("img",{src:"../../images/2025-06-27 23.17.57.jpg",className:"w-full h-130 object-cover rounded-lg md:max-w-md lg:max-w-lg mx-auto"}),`
+`,e.jsx("br",{}),`
+`,e.jsx(t.p,{children:"Summer has finally arrived. Outside, it’s already quite warm, about 30 degrees. I really like summer. During this time of year, people become different – much friendlier and more cheerful. All problems seem to fade into the background. Nature also comes to life and shines in vibrant colors."}),`
+`,e.jsx("br",{}),`
+`,e.jsx("img",{src:"../../images/2025-06-27 23.19.47.jpg",className:"w-full h-130 object-cover rounded-lg md:max-w-md lg:max-w-lg mx-auto"}),`
+`,e.jsx("br",{}),`
+`,e.jsx(t.p,{children:"The sun is the best medicine for all forms of life. Sometimes I believe that diseases are terribly afraid of the sun. All living beings seem to be recharged by the sun; they need it just like a phone needs electricity. The sun makes nature better, brings many colors and sugar-sweet fruits. Thanks to the sun and rain, sweet wild strawberries grow in the garden."}),`
+`,e.jsx("br",{}),`
+`,e.jsx("img",{src:"../../images/2025-06-27 23.19.08.jpg",className:"w-full h-130 object-cover rounded-lg md:max-w-md lg:max-w-lg mx-auto"}),`
+`,e.jsx("br",{}),`
+`,e.jsx(t.p,{children:`One day, I received an invitation to pick wild strawberries. That was a big and clear surprise for me. But I didn’t yet know how many berries there would be or how much strength and patience the harvest would require from me. I truly didn’t know. On a set day, I made a harvesting appointment and began the picking.
+It was a magical morning – the whole garden was decorated with ripe berries, like a room with a red carpet. I felt like a little dwarf who found themselves in a fairytale.`}),`
+`,e.jsx("br",{}),`
+`,e.jsx("img",{src:"../../images/2025-06-27 23.18.46.jpg",className:"w-full h-130 object-cover rounded-lg md:max-w-md lg:max-w-lg mx-auto"}),`
+`,e.jsx("br",{}),`
+`,e.jsx(t.p,{children:"It was a beautiful morning – lovely and exhausting at the same time. My legs often told me they were tired and couldn’t go any further. My eyes could no longer recognize the red berries and saw no more. I took short breaks with wild strawberry snacks and kept going. They smelled so good and tasted even better. That filled me again with strength and patience, giving me the courage and mood to continue harvesting."}),`
+`,e.jsx("br",{}),`
+`,e.jsx("img",{src:"../../images/2025-06-27 23.19.13.jpg",className:"w-full h-130 object-cover rounded-lg md:max-w-md lg:max-w-lg mx-auto"}),`
+`,e.jsx("br",{}),`
+`,e.jsx(t.p,{children:"At the end of the day, I had about 2 kilograms of sugar-sweet and magically fragrant wild strawberries. The berries filled my whole home with their wonderful aroma. But I didn’t yet know what to do with so many berries. To buy a little time, I put them in the fridge until the evening. By the evening, I already knew: I wanted to dry a quarter of them, eat a small portion raw, and turn the rest into magical jam."}),`
+`,e.jsx("br",{}),`
+`,e.jsx("img",{src:"../../images/2025-06-27 23.18.28.jpg",className:"w-full h-130 object-cover rounded-lg md:max-w-md lg:max-w-lg mx-auto"}),`
+`,e.jsx("br",{}),`
+`,e.jsx(t.p,{children:"Many thanks to a kind Swiss family who gave me the green light to pick the wild strawberries. It took four hours of my time and required a lot of effort and patience from me, but it was worth it."}),`
+`,e.jsx("br",{}),`
+`,e.jsx("img",{src:"../../images/2025-06-27 23.16.33.jpg",className:"w-full h-130 object-cover rounded-lg md:max-w-md lg:max-w-lg mx-auto"}),`
+`,e.jsx("br",{})]})}function i(a={}){const{wrapper:t}=a.components||{};return t?e.jsx(t,{...a,children:e.jsx(r,{...a})}):r(a)}export{i as default,s as frontmatter};
